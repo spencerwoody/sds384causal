d51162d3f4a06b039e59e00a5ac2faafa1c177c3
Spencer Woody <spencer.woody@utexas.edu>
2020-03-06 11:22:34 -0600

    Finish adding text of homework assignment

--- homework2/nox.R
+++ homework2/nox.R
@@ -1,3 +1,15 @@
 
 library(ggplot2)
 library(cowplot)
+library(dplyr)
+library(tidyr)
+
+theme_set(theme_half_open())
+
+###############################################################################
+                                        #          Read and prep data         #
+###############################################################################
+
+nox <- read.csv("annualEGUs.csv", header = TRUE, stringsAsFactors = FALSE)
+
+glimpse(nox)
